1757ae3074830f7d7cf201af03cafdc6048130fb
Arin Paliwal <paliwalarin09@gmail.com>
2024-11-28 10:26:34 +0530

    Added Test Driven Development (TDD)

--- November-24/28-11-2024/unit-testing/src/intro.js
+++ November-24/28-11-2024/unit-testing/src/intro.js
@@ -5,8 +5,13 @@ export function max(a, b) {
 
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n.toString();
+}
+
+export function calculateAverage(numbers) {
+  if (numbers.length == 0) return NaN;
+  return numbers.reduce((sum, current) => sum + current, 0) / numbers.length;
 }
--- November-24/28-11-2024/unit-testing/tests/intro.test.js
+++ November-24/28-11-2024/unit-testing/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the greater number when the first number is greater", () => {
@@ -26,5 +26,22 @@ describe("fizzBuzz", () => {
   it("should return the number as a string when the number is not divisible by 3 or 5", () => {
     const number = 77;
     expect(fizzBuzz(number)).toBe(number.toString());
+  });
+});
+
+// Test Driven Development (TDD)
+
+describe("calculateAverage", () => {
+  it("should return NaN when the array is empty", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+  it("should calculate average of array with single element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+  it("should calculate average of array with two elements", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+  it("should calculate average of array with multiple elements", () => {
+    expect(calculateAverage([1, 2, 3, 4, 5])).toBe(3);
   });
 });
