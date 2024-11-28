57175b8c65c6fce1b9edbcf149b352479edcfd9a
Arin Paliwal <paliwalarin09@gmail.com>
2024-11-28 10:31:53 +0530

    Added Unit Testing Module

--- November-24/28-11-2024/unit-testing/tests/intro.test.js
+++ November-24/28-11-2024/unit-testing/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { calculateAverage, fizzBuzz, max } from "../src/intro";
+import { calculateAverage, factorial, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the greater number when the first number is greater", () => {
@@ -30,6 +30,9 @@ describe("fizzBuzz", () => {
 });
 
 // Test Driven Development (TDD)
+// - It prevents over engineering by writing only the code that is needed to make the test pass.
+// - It helps to write clean and maintainable code by writing tests first.
+// - It helps us to write code that is 100% covered by tests.
 
 describe("calculateAverage", () => {
   it("should return NaN when the array is empty", () => {
@@ -43,5 +46,20 @@ describe("calculateAverage", () => {
   });
   it("should calculate average of array with multiple elements", () => {
     expect(calculateAverage([1, 2, 3, 4, 5])).toBe(3);
+  });
+});
+
+describe("factorial", () => {
+  it("should return 1 when the number is 0", () => {
+    expect(factorial(0)).toBe(1);
+  });
+  it("should return 1 when the number is 1", () => {
+    expect(factorial(1)).toBe(1);
+  });
+  it("should return the factorial of the number", () => {
+    expect(factorial(5)).toBe(120);
+  });
+  it("should return undefined when the number is negative", () => {
+    expect(factorial(-4)).toBeUndefined();
   });
 });
