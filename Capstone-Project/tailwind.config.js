e2e9b26ab8de8b0b06293171d11b99edf3d9b963
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-01 23:44:42 +0530

    Added Color Pallete

--- Capstone-Project/tailwind.config.js
+++ Capstone-Project/tailwind.config.js
@@ -1,25 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
-        palette: {
-          light: {
-            blue: "#3C41E9",
-            green: "#12A779",
-            orange: "#FFA40A",
-            purple: "#913AF6",
-            lightBlue: "#427DDC",
-          },
-          dark: {
-            blue: "#1F22A7",
-            green: "#0C7B5A",
-            orange: "#CC8308",
-            purple: "#732BBF",
-            lightBlue: "#315BB0",
-          },
+        lightMode: {
+          background: "#fafafa",
+          secondaryBackground: "#f0f0f0",
+          primaryText: "#18181b",
+          secondaryText: "#71717a",
+          accentBlue: "#3C41E9",
+          accentGreen: "#12A779",
+          accentOrange: "#FFA40A",
+          accentPurple: "#913AF6",
+          accentLightBlue: "#427DDC",
+        },
+        darkMode: {
+          background: "#09090b",
+          secondaryBackground: "#161617",
+          primaryText: "#ffffff",
+          secondaryText: "#a1a1aa",
+          accentBlue: "#1F22A7",
+          accentGreen: "#0C7B5A",
+          accentOrange: "#CC8308",
+          accentPurple: "#732BBF",
+          accentLightBlue: "#315BB0",
+        },
+        borders: {
+          primary: "#1e1e1e",
+          secondary: "#525252",
         },
       },
     },
