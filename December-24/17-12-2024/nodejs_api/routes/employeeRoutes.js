fda97b4bd948d8b1a2cdfa505f43a495d18b5ab1
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-17 20:37:21 +0530

    Added PUT DELETE Routes

--- December-24/17-12-2024/nodejs_api/routes/employeeRoutes.js
+++ December-24/17-12-2024/nodejs_api/routes/employeeRoutes.js
@@ -21,4 +21,38 @@ employeeRouter.post("/add", (req, res) => {
   });
 });
 
+employeeRouter.put("/update/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const userIndex = employees.findIndex((user) => user.id == id);
+  if (userIndex === -1) {
+    return res.status(404).json({
+      status: 404,
+      message: "Employee not found",
+    });
+  }
+  employees[userIndex] = { ...employees[userIndex], ...req.body };
+  res.status(200).json({
+    status: 200,
+    message: "Employee updated successfully",
+    data: employees[userIndex],
+  });
+});
+
+
+employeeRouter.delete("/delete/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const userIndex = employees.findIndex((user) => user.id == id);
+  if (userIndex === -1) {
+    return res.status(404).json({
+      status: 404,
+      message: "Employee not found",
+    });
+  }
+  employees.splice(userIndex, 1);
+  res.status(200).json({
+    status: 200,
+    message: "Employee deleted successfully",
+  });
+});
+
 export default employeeRouter;
