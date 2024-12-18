677bcfc579866ea52b8a00aa26c236fd25ba2b3c
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-18 12:07:27 +0530

    Added Controllers

---
+++ December-24/17-12-2024/nodejs_api/controllers/employeeController.js
@@ -0,0 +1,53 @@
+import { employees } from "../data/index.js";
+
+export const getEmployees = (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Employees fetched successfully",
+    data: employees,
+  });
+};
+
+export const addEmployee = (req, res) => {
+  const newUser = req.body;
+  newUser.id = employees.length + 1;
+  employees.push(newUser);
+  res.status(201).json({
+    status: 201,
+    message: "Employee added successfully",
+    data: newUser,
+  });
+};
+
+export const updateEmployee = (req, res) => {
+  const id = parseInt(req.params.id);
+  const userIndex = employees.findIndex((user) => user.id == id);
+  if (userIndex === -1) {
+    return res.status(404).json({
+      status: 404,
+      message: "Employee not found",
+    });
+  }
+  employees[userIndex] = { ...employees[userIndex], ...req.body };
+  res.status(200).json({
+    status: 200,
+    message: "Employee updated successfully",
+    data: employees[userIndex],
+  });
+};
+
+export const deleteEmployee = (req, res) => {
+  const id = parseInt(req.params.id);
+  const userIndex = employees.findIndex((user) => user.id == id);
+  if (userIndex === -1) {
+    return res.status(404).json({
+      status: 404,
+      message: "Employee not found",
+    });
+  }
+  employees.splice(userIndex, 1);
+  res.status(200).json({
+    status: 200,
+    message: "Employee deleted successfully",
+  });
+};
--- December-24/17-12-2024/nodejs_api/routes/employeeRoutes.js
+++ December-24/17-12-2024/nodejs_api/routes/employeeRoutes.js
@@ -1,58 +1,16 @@
 import { Router } from "express";
-import {employees} from "../data/index.js"
+import {
+  getEmployees,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from "../controllers/employeeController.js";
+
 const employeeRouter = Router();
 
-employeeRouter.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    message: "Employees fetched successfully",
-    data: employees,
-  });
-});
-
-employeeRouter.post("/add", (req, res) => {
-  const newUser= req.body;
-  newUser.id=employees.length+1;
-  employees.push(newUser);
-  res.status(201).json({
-    status: 201,
-    message: "Employee added successfully",
-    data: newUser,
-  });
-});
-
-employeeRouter.put("/update/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const userIndex = employees.findIndex((user) => user.id == id);
-  if (userIndex === -1) {
-    return res.status(404).json({
-      status: 404,
-      message: "Employee not found",
-    });
-  }
-  employees[userIndex] = { ...employees[userIndex], ...req.body };
-  res.status(200).json({
-    status: 200,
-    message: "Employee updated successfully",
-    data: employees[userIndex],
-  });
-});
-
-
-employeeRouter.delete("/delete/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const userIndex = employees.findIndex((user) => user.id == id);
-  if (userIndex === -1) {
-    return res.status(404).json({
-      status: 404,
-      message: "Employee not found",
-    });
-  }
-  employees.splice(userIndex, 1);
-  res.status(200).json({
-    status: 200,
-    message: "Employee deleted successfully",
-  });
-});
+employeeRouter.get("/", getEmployees);
+employeeRouter.post("/add", addEmployee);
+employeeRouter.put("/update/:id", updateEmployee);
+employeeRouter.delete("/delete/:id", deleteEmployee);
 
 export default employeeRouter;
